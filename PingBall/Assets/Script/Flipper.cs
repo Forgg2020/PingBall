b5a73f3038fddeb7187054b693aed8f3ae838704
forgg2020 <billlin87823@gmail.com>
2023-04-10 19:00:38 +0900

    Add ignore

---
+++ PingBall/Assets/Script/Flipper.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    public float reatPosition = 0f;
+    public float pressedPosition = 45f;
+    public float hitStrenght = 10000f;
+    public float flipperDamper = 150f;
+
+    public string inputName;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //if(Input.GetAxis(inputName) == 1)
+        //{
+
+        //}
+    }
+}
