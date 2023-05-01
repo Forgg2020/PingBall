03c9545ba369f4126c18815e3890db3197e914e3
Forgg2020 <billlin87823@gmail.com>
2023-05-01 19:55:22 +0900

    add camera movement

--- PingBall/Assets/Script/Level/EndGame.cs
+++ PingBall/Assets/Script/Level/EndGame.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc906fbdd5c66120968c3d91748c3a26bb6bf355e43ad6de0dae1443cfbdf105
-size 1323
+oid sha256:caba5d9af937ebd3127591043697185bbb1936183e37c14480027e324821c340
+size 1890
