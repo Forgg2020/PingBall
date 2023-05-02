5bf73dd6fce7f756acc61e953433078c4fe52fc3
Forgg2020 <billlin87823@gmail.com>
2023-05-02 13:05:06 +0900

    Rewrite telepoint function
    merger telepoint script to playerstate script, delete the former one

---
+++ PingBall/Assets/Script/Player/PlayerDeath.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c2702c99ac774cf13601b0adaa192e8be862ee1fb88f15f5f96290e491094ff
+size 827
--- PingBall/Assets/Script/Player/PlayerState.cs
+++ PingBall/Assets/Script/Player/PlayerState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86eac4af0dee7a550bd00f0aabd8a81eb6c71f7449633752517cdf90ea42457a
-size 751
+oid sha256:3c785b58ee992ba036c2598c5126bd80c1d150a96cdb4435630878ee3cb56bca
+size 1167
